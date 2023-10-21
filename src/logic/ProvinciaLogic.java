05f44cedea37ec343aee8368e14026f59555a36e
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-10-21 09:41:11 -0300

    Crear métodos de lógica #5  closes #4

--- src/logic/ProvinciaLogic.java
+++ src/logic/ProvinciaLogic.java
@@ -4,10 +4,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import data.ProvinciaDao;
 import entity.*;
 import max.data.*;
+import max.oops.SchemaValidationException;
 
 public class ProvinciaLogic implements IRecordLogic<Provincia, Integer> {
+	
+	private ProvinciaDao data = new ProvinciaDao();
 	
 	public ProvinciaLogic() {
 		// TODO Auto-generated constructor stub 
@@ -36,44 +40,101 @@ public class ProvinciaLogic implements IRecordLogic<Provincia, Integer> {
 
 	@Override
 	public LogicResponse<Provincia> delete(Provincia arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		LogicResponse<Provincia> res = new LogicResponse<Provincia>();
+		TransactionResponse<?> tn = data.delete(arg0);
+		if(tn.rowsAffected > 0) {
+			res.die(true, "El registro se eliminó con éxito. ");
+		} else res.die(false, "Hubo un error al intentar eliminar el registro. ");
+		return res;
+		
 	}
 
 	@Override
 	public LogicResponse<Provincia> exists(Integer arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		LogicResponse<Provincia> res = new LogicResponse<Provincia>();
+		boolean tn = false;
+		try {
+			tn = data.exists(arg0);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(tn) {
+			res.die(true, "El registro existe. ");
+		} else res.die(false, "No existe un registro con esas características. ");
+		return res;
 	}
 
 	@Override
 	public LogicResponse<Provincia> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		LogicResponse<Provincia> res = new LogicResponse<Provincia>();
+		TransactionResponse<Provincia> tn = new TransactionResponse<Provincia>();
+		try {
+			tn = data.getAll();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(tn.nonEmptyResult()) {
+			res.fill(tn.rowsReturned);
+		} else res.die(false, "Hubo un error al intentar realizar la consulta. ");
+		return res;
 	}
 
 	@Override
 	public LogicResponse<Provincia> getById(Integer arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		LogicResponse<Provincia> res = new LogicResponse<Provincia>();
+		TransactionResponse<Provincia> tn = new TransactionResponse<Provincia>();
+		try {
+			tn = data.getById(arg0);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(tn.nonEmptyResult()) {
+			res.fill(tn.rowsReturned);
+		} else res.die(false, "Hubo un error al intentar realizar la consulta. ");
+		return res;
 	}
 
 	@Override
 	public LogicResponse<Provincia> insert(Provincia arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		LogicResponse<Provincia> res = new LogicResponse<Provincia>();
+		TransactionResponse<?> tn = data.insert(arg0);
+		if(tn.rowsAffected > 0) {
+			res.die(true, "El registro se insertó con éxito. ");
+		} else res.die(false, "Hubo un error al intentar insertar el registro. ");
+		return res;
 	}
 
 	@Override
 	public LogicResponse<Provincia> modify(Provincia arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		LogicResponse<Provincia> res = new LogicResponse<Provincia>();
+		TransactionResponse<?> tn = data.modify(arg0);
+		if(tn.rowsAffected > 0) {
+			res.die(true, "El registro se modificó con éxito. ");
+		} else res.die(false, "Hubo un error al intentar modificar el registro. ");
+		return res;
 	}
 
+	/**
+	 * Valida un objeto Provincia.
+	 * Si "validateConstraints" es falso, se valida usando el método Schema.validate().
+	 * Si "validateConstraints" es verdadero, se valida usando el método IModel.validate(), que incluye validaciones en la base de datos.
+	 */
 	@Override
-	public LogicResponse<Provincia> validate(Provincia arg0, boolean arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public LogicResponse<Provincia> validate(Provincia arg0, boolean validateConstraints) {
+		LogicResponse<Provincia> res = new LogicResponse<Provincia>();
+		try {
+			res.status = validateConstraints 
+					? Provincia._model.validate(arg0.toDictionary()) 
+					: Provincia._schema.validate(arg0.toDictionary());
+		} catch (SchemaValidationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			res.die(false, e.getMessage());
+		}
+		return res;
 	}
 	
 	
