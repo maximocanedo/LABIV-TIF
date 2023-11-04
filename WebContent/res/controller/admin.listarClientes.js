b8a3a3a8dda3a6bea8cd529439786c0c307c2dc5
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-04 10:50:39 -0300

    Prototipo de búsqueda en frontend

--- WebContent/res/controller/admin.listarClientes.js
+++ WebContent/res/controller/admin.listarClientes.js
@@ -2,10 +2,21 @@
 import * as material from "./../controller/mdc.controller.js";
 import * as provinces from "./../data/provinces.js";
 import * as localties from "./../data/localties.js";
+import * as countries from "./../data/countries.js";
 import * as auth from "./../data/auth.js";
 
-const getData = async () => {
-	return fetch("http://localhost:8080/TPINT_GRUPO_3_LAB/api/clients", {
+const getData = async (data = null) => {
+	let ue = "?";
+	if (data != null) {
+		// Filtrar propiedades con valores no nulos
+		const filteredData = Object.fromEntries(
+			Object.entries(data).filter(([key, value]) => value !== null)
+		);
+
+		// Convertir a cadena de consulta
+		ue += new URLSearchParams(filteredData).toString();
+	}
+	return fetch("http://localhost:8080/TPINT_GRUPO_3_LAB/api/clients" + ue, {
 		headers: auth.AUTH_HEADER,
 		method: "GET",
 	})
@@ -26,10 +37,59 @@ const getData = async () => {
 		});
 };
 
+const searchEvent = async (e) => {
+	const q = document.searchForm.q.value;
+	const status = !document.searchForm.showInactive.checked;
+	const province =
+		document.searchForm.provincias.value == "-1"
+			? null
+			: document.searchForm.provincias.value;
+	const localty =
+		document.searchForm.localidades.value == "-1"
+			? null
+			: document.searchForm.localidades.value;
+	const sex =
+		document.searchForm.sexo.value == "M" ||
+		document.searchForm.sexo.value == "F"
+			? document.searchForm.sexo.value
+			: null;
+	const country =
+		document.searchForm.pais.value == "-1"
+			? null
+			: document.searchForm.pais.value;
+	const data = {
+		q,
+		status,
+		province,
+		localty,
+		sex,
+		country,
+	};
+	const fetchedData = await getData(data);
+	fillData(fetchedData);
+};
+
+const fillData = (data) => {
+	// Diseñar luego:
+	document.querySelector("ul#listaClientes").innerHTML = "";
+	data.listReturned.forEach((element) => {
+		const item = document.createElement("li");
+		const a = document.createElement("a");
+		a.setAttribute("href", "/clientes/perfil.html?user=" + element.usuario);
+		a.innerText =
+			element.nombre + " " + element.apellido + " (" + element.DNI + ") ";
+		item.append(a);
+		document.querySelector("ul#listaClientes").append(item);
+	});
+};
+
 (async () => {
 	material.loadElements();
 	const actualUser = await auth.allowAdmin();
 	const data = await getData();
+	document.searchForm.searchBtn.addEventListener("click", async () => {
+		await searchEvent();
+	});
 	// Cargar controles:
 	const provincias = await provinces.getProvinces();
 	console.log({ provincias });
@@ -39,8 +99,18 @@ const getData = async () => {
 		option.value = opcion.id;
 		document.searchForm.provincias.appendChild(option);
 	});
+
+	const paises = await countries.getCountries();
+	console.log({ paises });
+	paises.forEach((opcion) => {
+		const option = document.createElement("option");
+		option.text = opcion.nombre;
+		option.value = opcion.codigo;
+		document.searchForm.pais.appendChild(option);
+	});
+
 	document.searchForm.provincias.addEventListener("change", async (event) => {
-		const provinceId = event.srcElement.value;
+		const provinceId = document.searchForm.provincias.value;
 		const localidades = await localties.getLocalties(provinceId);
 		document.searchForm.localidades.innerHTML = "";
 		const defOp = document.createElement("option");
@@ -55,14 +125,5 @@ const getData = async () => {
 		});
 	});
 
-	// Diseñar luego:
-	data.listReturned.forEach((element) => {
-		const item = document.createElement("li");
-		const a = document.createElement("a");
-		a.setAttribute("href", "/clientes/perfil.html?user=" + element.usuario);
-		a.innerText =
-			element.nombre + " " + element.apellido + " (" + element.DNI + ") ";
-		item.append(a);
-		document.querySelector("ul#listaClientes").append(item);
-	});
+	fillData(data);
 })();
