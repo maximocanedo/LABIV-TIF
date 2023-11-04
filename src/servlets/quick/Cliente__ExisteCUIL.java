1a0651c74d9be508b29bbc3fc08e6d34140024e7
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-04 11:01:53 -0300

    Datos rápidos de clientes

---
+++ src/servlets/quick/Cliente__ExisteCUIL.java
@@ -0,0 +1,45 @@
+package servlets.quick;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entity.Cliente;
+import logic.ClienteLogic;
+import max.data.Response;
+
+/**
+ * Servlet implementation class Cliente__ExisteCUIL
+ */
+@WebServlet("/api/quick/client/cuil/*")
+public class Cliente__ExisteCUIL extends servlets.BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Cliente__ExisteCUIL() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    private ClienteLogic AL = new ClienteLogic();
+
+    public void getAdmin(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    	String dni = getPathParameter(request);
+    	Response<Cliente> result = AL.CUILExists(dni);
+    	sendEmptyResponse(response, result.http);
+    	return; 
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getAdmin(request, response);
+	}
+
+
+	@Override
+	protected String[] getAllowedMethods() {
+		return new String[] { "GET" };
+	}
+
+}
---
+++ src/servlets/quick/Cliente__ExisteDNI.java
@@ -0,0 +1,44 @@
+package servlets.quick;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entity.Cliente;
+import logic.ClienteLogic;
+import max.data.Response;
+
+/**
+ * Servlet implementation class Cliente__ExisteDNI
+ */
+@WebServlet("/api/quick/client/dni/*")
+public class Cliente__ExisteDNI extends servlets.BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Cliente__ExisteDNI() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    private ClienteLogic AL = new ClienteLogic();
+
+    public void getAdmin(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    	String dni = getPathParameter(request);
+    	Response<Cliente> result = AL.DNIExists(dni);
+    	response.setStatus(result.http);
+    	sendEmptyResponse(response, result.http);
+    	return; 
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getAdmin(request, response);
+	}
+
+
+	@Override
+	protected String[] getAllowedMethods() {
+		return new String[] { "GET", "OPTIONS" };
+	}
+
+}
