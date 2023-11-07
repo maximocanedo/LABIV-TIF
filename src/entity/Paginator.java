243f35017b1cf5d734333a114173b614494d45b3
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-07 19:26:35 -0300

    Métodos útiles

--- src/entity/Paginator.java
+++ src/entity/Paginator.java
@@ -1,17 +1,19 @@
 package entity;
 
+import com.google.gson.JsonObject;
+
 public class Paginator {
 	// Cantidad de elementos por página.
 	private int size; 
 	// Página actual. Comienza siempre por 0.
 	private int page; 
 	// Cantidad de páginas en total.
-	private int totalPages;
+	private int total;
 	
 	public Paginator(int size, int page, int totalPages) {
 		this.size = size;
 		this.page = page;
-		this.totalPages = totalPages;
+		this.total = totalPages;
 	}
 	public Paginator(int size, int page) {
 		this(size, page, 0);
@@ -20,6 +22,15 @@ public class Paginator {
 		this(size, 1);
 	}
 	public Paginator() {}
+	
+	public JsonObject toJsonObject() {
+		JsonObject obj = new JsonObject();
+		obj.addProperty("size", size);
+		obj.addProperty("page", page);
+		obj.addProperty("total", total);
+		return obj;
+	}
+	
 	public int getSize() {
 		return size;
 	}
@@ -33,10 +44,10 @@ public class Paginator {
 		this.page = page;
 	}
 	public int getTotalPages() {
-		return totalPages;
+		return total;
 	}
 	public void setTotalPages(int totalPages) {
-		this.totalPages = totalPages;
+		this.total = totalPages;
 	}
 	
 	
