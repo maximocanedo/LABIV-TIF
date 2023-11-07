8618b31a9130c66ad1b938c7eba8f573c6d35a1d
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-07 10:27:52 -0300

    Interfaz IConceptoLogic #99

---
+++ src/logic/IConceptoLogic.java
@@ -0,0 +1,19 @@
+package logic;
+
+import java.util.List;
+
+import entity.Concepto;
+import max.data.Dictionary;
+import max.data.Response;
+
+public interface IConceptoLogic {
+	public Concepto convert(Dictionary data);
+	public List<Concepto> convert(List<Dictionary> list);
+	public Response<Concepto> delete(Concepto concepto);
+	public Response<Concepto> exists(String id);
+	public Response<Concepto> getAll();
+	public Response<Concepto> getById(String id);
+	public Response<Concepto> insert(Concepto concepto);
+	public Response<Concepto> modify(Concepto concepto);
+	public Response<Concepto> validate(Concepto concepto, boolean validateConstraints);
+}
