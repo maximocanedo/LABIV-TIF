cffcf7b3f6c88020ed5943e3f25ccb8c105d7834
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-07 11:00:46 -0300

    Interfaz ITipoCuentaDao #99

--- src/dataImpl/TipoCuentaDao.java
+++ src/dataImpl/TipoCuentaDao.java
@@ -2,6 +2,8 @@ package dataImpl;
 
 import java.sql.SQLException;
 import java.sql.Types;
+
+import data.ITipoCuentaDao;
 import entity.TipoCuenta;
 import logicImpl.TipoCuentaLogic;
 import max.data.Dictionary;
@@ -14,7 +16,7 @@ import max.schema.IModel;
 import max.schema.MySQLSchemaModel;
 import max.schema.SchemaProperty;
 
-public class TipoCuentaDao implements IRecord<TipoCuenta, String>{
+public class TipoCuentaDao implements IRecord<TipoCuenta, String>, ITipoCuentaDao{
 
 	public TipoCuentaDao(){}
 	private TipoCuentaLogic tpLogic= new TipoCuentaLogic();
@@ -37,10 +39,17 @@ public class TipoCuentaDao implements IRecord<TipoCuenta, String>{
 	public static final IModel _model= new MySQLSchemaModel("TipoCuenta","tif",tablaTP);
 	private Connector db = new Connector(_model.getDatabaseName());
 	
+	/* (non-Javadoc)
+	 * @see dataImpl.ITipoCuentaDao#printTDB()
+	 */
+	@Override
 	public String printTDB() {
 		return _model.getDatabaseName() + "." + _model.getTableName();
 	}
 	
+	/* (non-Javadoc)
+	 * @see dataImpl.ITipoCuentaDao#insert(entity.TipoCuenta)
+	 */
 	@Override
 	public TransactionResponse<?> insert(TipoCuenta data) throws SQLException {
 		TransactionResponse<?> res = TransactionResponse.create();
@@ -52,6 +61,9 @@ public class TipoCuentaDao implements IRecord<TipoCuenta, String>{
 		return res;
 	}
 
+	/* (non-Javadoc)
+	 * @see dataImpl.ITipoCuentaDao#delete(entity.TipoCuenta)
+	 */
 	@Override
 	public TransactionResponse<?> delete(TipoCuenta data) throws SQLException {
 		TransactionResponse<?> res = null;
@@ -63,6 +75,9 @@ public class TipoCuentaDao implements IRecord<TipoCuenta, String>{
 		return res;
 	}
 
+	/* (non-Javadoc)
+	 * @see dataImpl.ITipoCuentaDao#modify(entity.TipoCuenta)
+	 */
 	@Override
 	public TransactionResponse<?> modify(TipoCuenta data) throws SQLException {
 		TransactionResponse<?> res = TransactionResponse.create();
@@ -74,6 +89,9 @@ public class TipoCuentaDao implements IRecord<TipoCuenta, String>{
 		return res;
 	}
 
+	/* (non-Javadoc)
+	 * @see dataImpl.ITipoCuentaDao#getAll()
+	 */
 	@Override
 	public TransactionResponse<TipoCuenta> getAll() throws SQLException {
 		TransactionResponse<Dictionary> rows= db.fetch(
@@ -86,6 +104,9 @@ public class TipoCuentaDao implements IRecord<TipoCuenta, String>{
 		return rowsTP;
 	}
 
+	/* (non-Javadoc)
+	 * @see dataImpl.ITipoCuentaDao#getById(java.lang.String)
+	 */
 	@Override
 	public TransactionResponse<TipoCuenta> getById(String Cod_TPCT) throws SQLException {
 		TransactionResponse<Dictionary> rows= db.fetch(
@@ -99,6 +120,9 @@ public class TipoCuentaDao implements IRecord<TipoCuenta, String>{
 		return rowsTP;
 	}
 
+	/* (non-Javadoc)
+	 * @see dataImpl.ITipoCuentaDao#exists(java.lang.String)
+	 */
 	@Override
 	public boolean exists(String Cod_TPCT) throws SQLException {
 		
