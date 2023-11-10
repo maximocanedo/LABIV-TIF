776195a828d50bbe2d54c048ea12c8bff16d7321
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-10 17:25:50 -0300

    Extender alcance de endpoint #105

--- src/servlets/Cuentas.java
+++ src/servlets/Cuentas.java
@@ -7,9 +7,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import entity.Administrador;
 import entity.Cliente;
 import entity.Cuenta;
+import entity.Paginator;
 import logicImpl.AuthManager;
+import logicImpl.AuthManager.TokenData;
 import logicImpl.CuentaLogicImpl;
 import max.Dictionary;
 import max.Response;
@@ -31,12 +34,28 @@ public class Cuentas extends BaseServlet implements Servlet {
      * Método GET: Listar cuentas (Admin only)
      */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Cliente cliente = AuthManager.getActualClient(request, response);
-		if(cliente != null) {
-			Response<Cuenta> res = CL.getAllFor(cliente);
-			response.setStatus(res.http);
-			write(response, res.toFinalJSON());
+		TokenData td = AuthManager.readToken(request);
+		if(td == null) {
+			response.setStatus(401);
+			return;
 		}
+		if(td.role.equals(AuthManager.ADMIN)) {
+			Administrador admin = AuthManager.getActualAdmin(request, response);
+			Paginator paginator = getPaginator(request);
+			if(admin != null) {
+				Response<Cuenta> res = CL.getAll(paginator);
+				response.setStatus(res.http);
+				write(response, res.toFinalJSON());
+			}
+		} else {
+			Cliente cliente = AuthManager.getActualClient(request, response);
+			if(cliente != null) {
+				Response<Cuenta> res = CL.getAllFor(cliente);
+				response.setStatus(res.http);
+				write(response, res.toFinalJSON());
+			}
+		}
+		return;
 	}
 
 	
@@ -53,8 +72,7 @@ public class Cuentas extends BaseServlet implements Servlet {
 
 	@Override
 	protected String[] getAllowedMethods() {
-		// TODO Auto-generated method stub
-		return null;
+		return new String[] { "GET", "POST", "OPTIONS" };
 	}
 
 }
