13deb4d3035db431cdb90ea16e4fa15738acca2f
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-15 13:12:09 -0300

    Pequeño detalle

--- src/servlets/CuotaPrestamo.java
+++ src/servlets/CuotaPrestamo.java
@@ -26,8 +26,8 @@ public class CuotaPrestamo extends BaseServlet implements Servlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String prestamoId = getPathParameter(request, 1);
+		
 	}
 
 	/**
