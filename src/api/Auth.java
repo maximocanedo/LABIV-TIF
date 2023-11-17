b611023f25458142357d2a84082a314f69a70063
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-17 07:23:20 -0300

    Organizar inicio de sesión closes #115

--- src/api/Auth.java
+++ src/api/Auth.java
@@ -1,41 +1,76 @@
 package api;
 
+import entity.Administrador;
+import entity.Cliente;
+import logicImpl.AdministradorLogicImpl;
+import logicImpl.AuthManager;
+import logicImpl.ClienteLogicImpl;
+import max.Dictionary;
+import max.Response;
+import servlets.BaseServlet;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Auth
- */
-@WebServlet("/api/auth")
-public class Auth extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Auth() {
+@WebServlet("/v2/auth")
+public class Auth extends BaseServlet {
+	private final AdministradorLogicImpl AL = new AdministradorLogicImpl();
+	protected final ClienteLogicImpl CL = new ClienteLogicImpl();
+	public Auth() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void loginAsAdmin(HttpServletResponse response, Dictionary parameters) throws IOException {
+		Response<Administrador> finalRes = AL.login(parameters);
+		if(finalRes.status) {
+			String tk = finalRes.eField;
+			if(tk != null) {
+				AuthManager.sendToken(response, tk);
+			}
+		}
+		response.setStatus(finalRes.http);
+		finalRes.clean(true);
+		response.getWriter().append(finalRes.toFinalJSON());
+	}
+	protected void loginAsClient(HttpServletResponse response, Dictionary parameters) throws IOException {
+		Response<Cliente> finalRes = CL.login(parameters);
+		if(finalRes.status) {
+			String tk = finalRes.eField;
+			if(tk != null) {
+				AuthManager.sendToken(response, tk);
+			}
+		}
+		response.setStatus(finalRes.http);
+		write(response, finalRes.toFinalJSON());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * POST /auth
+	 * Iniciar sesión
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		Dictionary parameters = getParameters(request);
+		if(parameters != null && parameters.$("role") != null) {
+			switch(parameters.$("role").toString()) {
+				case AuthManager.ADMIN:
+					loginAsAdmin(response, parameters);
+					return;
+				case AuthManager.CLIENT:
+					loginAsClient(response, parameters);
+					return;
+				default:
+					response.setStatus(400);
+			}
+		} else {
+			response.setStatus(400);
+		}
+	}
+
+	@Override
+	protected String[] getAllowedMethods() {
+		return new String[] { "POST", "OPTIONS" };
 	}
 
 }
