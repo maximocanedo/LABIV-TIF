01646d3ae90c9ba912b075e2a8c6baad54cb467e
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-18 11:20:47 -0300

    Organizar servlets de conceptos y tipo de movimientos

--- src/api/Concepts.java
+++ src/api/Concepts.java
@@ -1,7 +1,7 @@
 package api;
 
-import entity.TipoCuenta;
-import logicImpl.TipoCuentaLogicImpl;
+import entity.Concepto;
+import logicImpl.ConceptoLogicImpl;
 import max.Response;
 import servlets.BaseServlet;
 
@@ -11,14 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/v2/accounts/types")
-public class AccountTypes extends BaseServlet {
-    public AccountTypes() { super(); }
-    private final TipoCuentaLogicImpl TCLI = new TipoCuentaLogicImpl();
+@WebServlet("/v2/concepts")
+public class Concepts extends BaseServlet {
+    public Concepts() { super(); }
+    private final ConceptoLogicImpl CLI = new ConceptoLogicImpl();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Response<TipoCuenta> res = TCLI.getAll();
+        Response<Concepto> res = CLI.getAll();
         write(resp, res.toFinalJSON());
     }
 
