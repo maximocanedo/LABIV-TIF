ca34cbc76378a4ee5f312d0ca79a21a04cf79b5a
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-23 09:55:36 -0300

    Funciones de gestión de clientes

--- classes/artifacts/Exploded_WebApp/res/data/clients.js
+++ classes/artifacts/Exploded_WebApp/res/data/clients.js
@@ -1,4 +1,6 @@
 "use strict";
+import { AUTH_HEADER } from "./auth.js";
+
 const DNIExists = async (DNI) => {
     try {
         const response = await fetch(
@@ -47,4 +49,38 @@ const CUILExists = async (CUIL) => {
         return 500;
     }
 };
-export {DNIExists, userExists, CUILExists};
+const disable = async (username) => {
+    try {
+        const response = await fetch(
+            "http://localhost:8080/TPINT_GRUPO_3_LAB/v2/clients/user/" + username,
+            {
+                method: "DELETE",
+                headers: AUTH_HEADER
+            }
+        );
+        const { status } = response;
+        const data = await response.json();
+        return { status, data };
+    } catch(e) {
+        return { status: 500, data: e };
+        console.error(e);
+    }
+};
+const enable = async (username) => {
+    try {
+        const response = await fetch(
+            "http://localhost:8080/TPINT_GRUPO_3_LAB/v2/clients/user/" + username,
+            {
+                method: "POST",
+                headers: AUTH_HEADER
+            }
+        );
+        const { status } = response;
+        const data = await response.json();
+        return { status, data };
+    } catch(e) {
+        return { status: 500, data: e };
+        console.error(e);
+    }
+};
+export {DNIExists, userExists, CUILExists, disable, enable};
