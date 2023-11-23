f49cf8211d794a7ff6d76907cb6035e2bcfba265
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-11-23 16:15:50 -0300

    Delete classes/artifacts/Exploded_WebApp directory

--- classes/artifacts/Exploded_WebApp/res/controller/res.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-import * as material from "./../controller/mdc.controller.js";
-import * as auth from "./../data/auth.js";
-
-(async () => {
-    material.loadElements();
-    const actualUser = await auth.allowAdmin();
-    console.log({actualUser});
-})();
